8b697f1f937eaac5fbe17e235f475d21228d0829
fc410 <54721672+fc410@users.noreply.github.com>
2020-02-10 14:35:16 -0800

    added a login page and am currently working on the all page

--- app/__init__.py
+++ app/__init__.py
@@ -5,5 +5,8 @@ app = Flask(__name__)
 
 #Set the secret key to some random bytes.Keep this really secret!
 app.config.from_object(Config)
+app.jinja_env.auto_reload = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.run(debug=True, host='0.0.0.0')
  
 from app import routes
